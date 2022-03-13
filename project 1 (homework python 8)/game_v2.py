1da5df2db876ad600de2ed2cbd1ba9544c8c289a
Sorensson <b5w5q5@mail.ru>
2022-03-13 22:11:59 +0400

    initial commit

---
+++ project 1 (homework python 8)/game_v2.py
@@ -0,0 +1,59 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def optimal_predict(number: int = 1) -> int:
+    '''Игра компьютер угадает число меньше чем за 20 попыток'''
+
+    import numpy as np
+
+    min = 0
+    max = 101
+
+    number = np.random.randint(min, max)
+
+    count = 0
+
+    while True:
+        count += 1
+        mid = (min+max) // 2
+
+        if mid > number:
+            max = mid
+
+        elif mid < number:
+            min = mid
+
+        else:
+            if mid == number:
+                break  # конец игры выход из цикла
+    return count
+
+
+def score_game(optimal_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        optimal_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    random_array = np.random.randint(1, 101, size=(1000))
+# загадали список чисел
+
+    for number in random_array:
+        count_ls.append(optimal_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(optimal_predict)
